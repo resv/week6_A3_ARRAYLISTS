758549f23e9299fa5b37026cccb44652a0c6fa12
Adam Kim <akim456@gmail.com>
2018-08-01 22:41:08 -0400

    spending way too much to make results look better... need to finish up last question and move on

--- assignment3_1to12.java
+++ assignment3_1to12.java
@@ -19,7 +19,7 @@ public class assignment3_1to12 {
 
 		// ADDING ELEMENTS (COLORS)
 		colors.addAll(Arrays.asList("Red", "Green", "Orange", "White", "Black"));
-		colors2.addAll(Arrays.asList("Yellow", "Blue", "Pink"));
+		colors2.addAll(Arrays.asList("Yellow", "Blue", "Pink", "Black"));
 
 		// PRINTING ARRAYLIST
 		System.out.println("\nQUESTION 1 -----------------------");
@@ -34,7 +34,7 @@ public class assignment3_1to12 {
 		sorting(colors);
 		reversing(colors);
 		copying(colors, blankArr);
-		comparing(colors);
+		comparing(colors, colors2);
 		appending(colors, colors2);
 		looping(colors);
 		converting(regArr);
@@ -52,16 +52,17 @@ public class assignment3_1to12 {
 		System.out.println("(Printed all elements as a sentence)");
 	}
 
-	// 3.UPDATING : WRITE A METHOD TO CHANGE THE 5TH ELEMENT OF ARRAYLIST TO "Magenta"
+	// 3.UPDATING : WRITE A METHOD TO CHANGE THE 5TH ELEMENT OF ARRAYLIST TO
+	// "Magenta"
 	private static void updating(ArrayList<String> list) {
 		System.out.println("\nQUESTION 3 -----------------------");
 		list.set(4, "Magenta");
 		System.out.println(list);
 		System.out.println("(5th element was changed to \"Magenta\")");
 		// IMPLEMENTED TO RESET ARRAY FOR NEXT METHOD TO TEST
-		list.clear(); 
+		list.clear();
 		list.addAll(Arrays.asList("Red", "Green", "Orange", "White", "Black"));
-		
+
 	}
 
 	// 4.REMOVING : WRITE A METHOD TO REMOVE THE THIRD ELEMENT OF YOUR ARRAYLIST
@@ -71,7 +72,7 @@ public class assignment3_1to12 {
 		System.out.println(list);
 		System.out.println("(3rd element \"Orange\" was removed)");
 		// IMPLEMENTED TO RESET ARRAY FOR NEXT METHOD TO TEST
-		list.clear(); 
+		list.clear();
 		list.addAll(Arrays.asList("Red", "Green", "Orange", "White", "Black"));
 	}
 
@@ -79,6 +80,7 @@ public class assignment3_1to12 {
 	private static void searching(ArrayList<String> list, String colorToFind) {
 		System.out.println("\nQUESTION 5 -----------------------");
 		Boolean exists = false;
+		System.out.println("Current list contains : " + list);
 		for (String colors : list) {
 			if (colorToFind.equalsIgnoreCase(colors)) {
 				System.out.println(colorToFind + " was searched and found!");
@@ -101,37 +103,60 @@ public class assignment3_1to12 {
 	// 7.REVERSING : WRITE A METHOD TO REVERSE YOUR ARRAYLIST
 	private static void reversing(ArrayList<String> list) {
 		System.out.println("\nQUESTION 7 -----------------------");
-		//PUSHING THE LAST INDEX, POPPING THE LAST INDEX OF ARRAY.SIZE()
+
+		// PUSHING THE LAST INDEX, POPPING THE LAST INDEX OF ARRAY.SIZE()
 		System.out.println("Current ArrayList order : " + list);
-		for (int i = 0, k = list.size() - 1 ; i < k; i++) {
+
+		for (int i = 0, k = list.size() - 1; i < k; i++) {
 			list.add(i, list.remove(k));
 		}
 		System.out.println("New reversed order..." + list);
-		//RESETTING ARRAY TO ORIGINAL ORDER FOR RETESTING PURPOSES
+
+		// RESETTING ARRAY TO ORIGINAL ORDER FOR RETESTING PURPOSES
 		list.clear();
 		list.addAll(Arrays.asList("Red", "Green", "Orange", "White", "Black"));
+
+		// "Collections.reverse(list)" WOULD DO ALL OF THIS IN ONE LINE!
+		// BUT I WANTED TO TRY TO USE RECURSION
 	}
 
 	// 8.COPYING : WRITE A METHOD THAT WILL COPY THE CONTENTS OF ONE ARRAYLIST TO
 	// ANOTHER.
 	private static void copying(ArrayList<String> list, ArrayList<String> blankList) {
 		System.out.println("\nQUESTION 8 -----------------------");
+
+		// ENSURING THE NEW ARRAY EXISTS AND IS CLEAR OF ANY VALUES
+		blankList.clear();
+
+		// FOR EACH LOOP TO ITERATE THROUGH list ARRAYLIST AND THEN ADDING TO NEW LIST
 		for (String elements2copy : list) {
 			blankList.add(elements2copy);
 		}
-		System.out.println(blankList);
+		System.out.println(blankList); // PRINTING OUT COPIED LIST
 		System.out.println("(Copied elements from an ArrayList into a blank ArrayList)");
-		System.out.println("(Be advised addAll() will point to and not assign values, there for avoided use");
+
+		// BE ADVISED addAll() WILL POINT TO AND NOT ASSIGN VALUES, THERE FOR AVOIDED
+		// USE
 		// IMPLEMENTED TO RESET blankList ARRAY TO RESET TEST
 		blankList.clear();
-		
+
 	}
 
 	// 9.COMPARING : WRITE A METHOD THAT WILL COMPARE TWO ARRAYLISTS, PRINTING OUT
 	// WHICH ELEMENTS THEY HAVE IN COMMON.
-	private static void comparing(ArrayList<String> list) {
+	private static void comparing(ArrayList<String> list, ArrayList<String> list2) {
 		System.out.println("\nQUESTION 9 -----------------------");
 
+		// FOR ENHANCED LOOPS FOR BOTH ARRAYS AND COMPARING THEM THEN PRINTING IF
+		// .equalsIgnoreCase()
+		for (String elements1 : list) {
+			for (String elements2 : list2) {
+				if (elements1.equalsIgnoreCase(elements2)) {
+					System.out.println("\"" + elements1 + "\" Has been matched in both ArrayLists!");
+				}
+			}
+		}
+		System.out.println("(Printing out all matching elements in two ArrayLists)");
 	}
 
 	// 10.APPENDING : WRITE A METHOD THAT WILL APPEND THE CONTENTS OF ONE ARRAYLIST
@@ -140,7 +165,7 @@ public class assignment3_1to12 {
 		System.out.println("\nQUESTION 10 -----------------------");
 		list.addAll(list2);
 		System.out.println(list);
-		System.out.println("(Appended both arrays)");
+		System.out.println("(Appended both ArrayList)");
 		// IMPLEMENTED TO RESET list2(colors2) ARRAY TO RESET TEST
 		list2.clear();
 		list2.addAll(Arrays.asList("Yellow", "Blue", "Pink"));
@@ -150,10 +175,24 @@ public class assignment3_1to12 {
 	// ARRAYLIST USING A FOR-EACH LOOP.
 	private static void looping(ArrayList<String> list) {
 		System.out.println("\nQUESTION 11 -----------------------");
+
+		//FOR LOOP IS USED FOR ADDING COMMAS, BUT THERE IS 
+		//A CORRECT NESTED FOR EACH LOOP TO SATISFY REQUIREMENT
+		int counter = 1;
 		for (String element : list) {
-			System.out.println(element);
+			String comma = ". ";
+			if (counter != list.size()) {
+				comma = ", ";
+			} else if (counter == list.size()) {
+				System.out.printf("and ");
+
+			} else {
+				comma = ".";
+			}
+			System.out.printf(element + comma);
+			counter++;
 		}
-		System.out.println("(Printed out all the elements in the ArrayList)");
+		System.out.println("\r\n(Printed out all the elements in the ArrayList)");
 	}
 
 	// 12.CONVERTING : WRITE A METHOD THAT WILL CONVERT A REGULAR ARRAY TO AN
